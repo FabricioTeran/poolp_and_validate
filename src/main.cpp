e4f4c2c235e87c1776bf7b3903cd4328ef50284a
FabricioTeran <avtomatkva@gmail.com>
2025-01-21 15:44:04 -0400

    validate.hpp separado

--- src/main.cpp
+++ src/main.cpp
@@ -3,120 +3,15 @@
 #include<phnt.h>
 #include<iostream>
 #include<vector>
-#include <type_traits>
+#include"validate.hpp"
 #pragma comment(lib, "ntdll")
 
 using namespace std;
-
-template <typename TReturn>
-bool BAD_PTR(TReturn ptr) {
-    MEMORY_BASIC_INFORMATION mbi = { 0 };
-    bool b;
-    void* p = (void*)ptr;
-    if (::VirtualQuery(p, &mbi, sizeof(mbi))) {
-        DWORD mask = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
-        b = !(mbi.Protect & mask);
-        // check the page is not a guard page
-        if (mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) b = true;
-
-        if (b) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
-    cout << "\nBAD_PTR: VirtualQuery no funciono en:" << ptr;
-    return false;
-}
-
-//Tambien podria usar overloading
-enum VALIDATE_FUNC {
-    CHECK_FALSE_NULL,
-    CHECK_NTSTATUS,
-    CHECK_BAD_PTR,
-    CHECK_HANDLE,
-    NO_CHECK
-};
-template <class ArgT>
-ArgT valExp(VALIDATE_FUNC validateFunc, const char* id, ArgT expression) {
-    if (validateFunc == CHECK_FALSE_NULL) {
-        void* helper = &expression; //expression se guarda en el parametro de la funcion, tomamos su ubicacion y luego casteamos esa ubicacion
-        if (!(bool)helper) {        //Es la misma tecnica usada en hacking para castear estructuras en tiempo de ejecucion
-            cout << "\nFalse or Null Value: " << (bool)helper << ", id: " << id << ", GetLastError: " << std::dec << GetLastError();
-            abort();
-        }
-    }
-    else if (validateFunc == CHECK_NTSTATUS) {
-        void* helper = &expression;
-        if (!NT_SUCCESS((NTSTATUS)helper)) {
-            cout << "\nNTSTATUS: " << (NTSTATUS)helper << ", id: " << id;
-            abort();
-        }
-    }
-    else if (validateFunc == CHECK_BAD_PTR) {
-        void* helper = &expression;
-        if (BAD_PTR(helper)) {
-            cout << "\nBad-Pointer:" << helper << ",  id:" << id << ",  GetLastError:" << std::dec << GetLastError(); //Usamos std::dec para convertir la DWORD de GetLastError en decimal, los errores documentados en msdn estan en decimal (y hexadecimal entre parentesis)
-            abort();
-        }
-    }
-    else if (validateFunc == CHECK_HANDLE) {
-
-    }
-
-    return expression;
-}
-int iVal = 0;
-void validateArgs(const char* id) { return; } //Solo me faltaba el puto parametro id, la recursion de packs funciona
-template <typename FirstT, typename... RestT>
-void validateArgs(const char* id, FirstT first, RestT... rest) {
-    //No podemos usar vectores porque no hay vectores que acepten diferentes tipos para sus elementos
-    //Tampoco podemos usar tuplas porque no se pueden iterar en tiempo de ejecucion
-    //void* vec[sizeof...(RestT)] = { &rest... }; Con esto perdemos los tipos de los argumentos: https://stackoverflow.com/a/60355020
-    
-    void* helper = &first;
-    if (typeid(first) == typeid(HANDLE)) {
-        cout << "\nid:" << id << " x:" << helper << " count:" << iVal++;
-        valExp(CHECK_HANDLE, id, first);
-    }
-
-    /* No es codigo portable is_pointer_v
-    else if (is_pointer_v<>) { //Quizas al comparar FirstT siempre da verdadero
-        cout << "\n2id:" << id << " x:" << helper << " count:" << iVal++;
-        valExp(CHECK_BAD_PTR, id, first);
-    }*/
-
-    validateArgs(id, rest...);
-}
-
-template <typename FuncT, typename... ArgsT> //Para declarar templates que devuelven cualquier tipo usamos auto
-auto val(VALIDATE_FUNC returnValidateFunc, const char* skipArgValidate, const char* id, FuncT func, ArgsT... args) {
-    validateArgs(id, args...); //Falta implementar skipArgValidate
-
-    auto res = func(args...); //Con auto no podemos usar NULL, debemos usar el tipo correcto para los argumentos
-
-    valExp(returnValidateFunc, id, res);
-
-    return res;
-}
-
-template <class FuncT, class... ArgsT> //Si las funciones a validar son templates, es muy dificil crear una funcion template que acepte otros templates, ademas tendriamos que definir una nueva funcion para cada nuevo template usado, entonces es mejor llamar la funcion desde afuera y que esta funcion solo tome el resultado
-FuncT valTemplate(VALIDATE_FUNC returnValidateFunc, const char* skipArgValidate, const char* id, FuncT call, ArgsT... args) {
-    validateArgs(id, args...); //Falta implementar skipArgValidate
-
-    valExp(returnValidateFunc, id, call);
-
-    return call;
-}
-#define valTemp(returnValidateFunc, skipArgValidate, id, func, ...) valTemplate(returnValidateFunc, skipArgValidate, id, func(__VA_ARGS__), __VA_ARGS__);
 
 typedef struct __PUBLIC_OBJECT_TYPE_INFORMATION {
     UNICODE_STRING TypeName;
     ULONG Reserved[22];    // reserved for internal use
 } PUBLIC_OBJECT_TYPE_INFORMATION, * PPUBLIC_OBJECT_TYPE_INFORMATION;
-
 
 template <typename TFunc, typename TInfoClass>
 vector<BYTE> queryInfo(HANDLE hProcess, TFunc queryFunc, TInfoClass processInfoClass) {        //QueryFUnctionArgs son *p_hTarget y ProcessHandleInformation=51 de la funcion llamada en handleHijacker.cpp
---
+++ src/validate.hpp
@@ -0,0 +1,113 @@
+#ifndef VALIDATE_H
+#define VALIDATE_H
+
+#include<iostream>
+#include<type_traits>
+#include<phnt_windows.h>
+#include<phnt.h>
+
+using namespace std;
+
+template <typename TReturn>
+bool BAD_PTR(TReturn ptr) {
+    MEMORY_BASIC_INFORMATION mbi = { 0 };
+    bool b;
+    void* p = (void*)ptr;
+    if (::VirtualQuery(p, &mbi, sizeof(mbi))) {
+        DWORD mask = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
+        b = !(mbi.Protect & mask);
+        // check the page is not a guard page
+        if (mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) b = true;
+
+        if (b) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    cout << "\nBAD_PTR: VirtualQuery no funciono en:" << ptr;
+    return false;
+}
+
+enum VALIDATE_FUNC {
+    CHECK_FALSE_NULL,
+    CHECK_NTSTATUS,
+    CHECK_BAD_PTR,
+    CHECK_HANDLE,
+    NO_CHECK
+};
+template <class ArgT>
+ArgT valExp(VALIDATE_FUNC validateFunc, const char* id, ArgT expression) {
+    if (validateFunc == CHECK_FALSE_NULL) {
+        void* helper = &expression; //expression se guarda en el parametro de la funcion, tomamos su ubicacion y luego casteamos esa ubicacion
+        if (!(bool)helper) {        //Es la misma tecnica usada en hacking para castear estructuras en tiempo de ejecucion
+            cout << "\nFalse or Null Value: " << (bool)helper << ", id: " << id << ", GetLastError: " << std::dec << GetLastError();
+            abort();
+        }
+    }
+    else if (validateFunc == CHECK_NTSTATUS) {
+        void* helper = &expression;
+        if (!NT_SUCCESS((NTSTATUS)helper)) {
+            cout << "\nNTSTATUS: " << (NTSTATUS)helper << ", id: " << id;
+            abort();
+        }
+    }
+    else if (validateFunc == CHECK_BAD_PTR) {
+        void* helper = &expression;
+        if (BAD_PTR(helper)) {
+            cout << "\nBad-Pointer:" << helper << ",  id:" << id << ",  GetLastError:" << std::dec << GetLastError(); //Usamos std::dec para convertir la DWORD de GetLastError en decimal, los errores documentados en msdn estan en decimal (y hexadecimal entre parentesis)
+            abort();
+        }
+    }
+    else if (validateFunc == CHECK_HANDLE) {
+
+    }
+
+    return expression;
+}
+
+extern int iVal = 0;
+void validateArgs(const char* id) { return; } //Solo me faltaba el puto parametro id, la recursion de packs funciona
+template <typename FirstT, typename... RestT>
+void validateArgs(const char* id, FirstT first, RestT... rest) {
+    void* helper = &first;
+    if (typeid(first) == typeid(HANDLE)) {
+        cout << "\nid:" << id << " x:" << helper << " count:" << iVal++;
+        valExp(CHECK_HANDLE, id, first);
+    }
+
+    /* No es codigo portable is_pointer_v
+    else if (is_pointer_v<FirstT>) { //Quizas al comparar FirstT siempre da verdadero porque FirstT es un grupo de tipos, no solo 1 tipo
+        cout << "\n2id:" << id << " x:" << helper << " count:" << iVal++;
+        valExp(CHECK_BAD_PTR, id, first);
+    }*/
+
+    validateArgs(id, rest...);
+}
+
+template <typename FuncT, typename... ArgsT> //Para declarar templates que devuelven cualquier tipo usamos auto
+auto val(VALIDATE_FUNC returnValidateFunc, const char* skipArgValidate, const char* id, FuncT func, ArgsT... args) {
+    validateArgs(id, args...); //Falta implementar skipArgValidate
+
+    auto res = func(args...); //Con auto no podemos usar NULL, debemos usar el tipo correcto para los argumentos
+
+    valExp(returnValidateFunc, id, res);
+
+    return res;
+}
+
+template <class FuncT, class... ArgsT> //Si las funciones a validar son templates, es muy dificil crear una funcion template que acepte otros templates, ademas tendriamos que definir una nueva funcion para cada nuevo template usado, entonces es mejor llamar la funcion desde afuera y que esta funcion solo tome el resultado
+FuncT valTemplate(VALIDATE_FUNC returnValidateFunc, const char* skipArgValidate, const char* id, FuncT call, ArgsT... args) {
+    validateArgs(id, args...); //Falta implementar skipArgValidate
+
+    valExp(returnValidateFunc, id, call);
+
+    return call;
+}
+
+
+#define valTemp(returnValidateFunc, skipArgValidate, id, func, ...) valTemplate(returnValidateFunc, skipArgValidate, id, func(__VA_ARGS__), __VA_ARGS__);
+
+#endif
